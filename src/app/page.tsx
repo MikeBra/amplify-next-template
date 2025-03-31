91322f3d7e14fce7419c20c749aa2c4e21dfc016
MikeBra <mhbracewell@gmail.com>
2025-03-30 21:06:08 -0500

    Moved amplify_outputs.json to the default root location to address build failures on Amplify.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Amplify } from "aws-amplify"
-import outputs from "@/config/amplify_outputs.json"
+import outputs from "../../amplify_outputs.json"
 import "@aws-amplify/ui-react/styles.css"
 import TodoList from "./components/TodoList"
 
--- src/config/amplify.ts
+++ src/config/amplify.ts
@@ -1,5 +1,5 @@
 import { Amplify } from "aws-amplify"
-import outputs from "@/config/amplify_outputs.json"
+import outputs from "../../amplify_outputs.json"
 
 export function configureAmplify() {
 	Amplify.configure(outputs)
